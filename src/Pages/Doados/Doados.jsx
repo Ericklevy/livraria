3ff462a4a13f16d9f2510a1a78a5bf6f7f7f16aa
Ericklevy <erickfga23@gmail.com>
2024-12-02 19:59:15 -0300

    adicionado o responsivo

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,8 +1,21 @@
-
+import S from './doados.module.scss';
+import PROTAGONISTA from '../../assets/protagonista.png';
 
 export default function Doados() {
     return (
-        <section>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.Card}>
+                <article >
+                    <img src={PROTAGONISTA} alt="imagem do livro protagonista" />
+                    <div>
+                        <h4>O protagonista</h4>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+
+                </article>
+            </section>
         </section>
     );
 }
