95b0a3a6e935ba24a5ff5815a400db6751b1d6b0
Ericklevy <erickfga23@gmail.com>
2025-03-31 11:22:46 -0300

    atualização do front

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,25 @@
 import S from './doados.module.scss';
 import PROTAGONISTA from '../../assets/protagonista.png';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function Doados() {
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const response = await axios.get("https://testeapiflask-reox.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(() => {
+        getLivros()
+    }, [])
+
+
+
+
     return (
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
@@ -13,8 +31,20 @@ export default function Doados() {
                         <p>Susanne Andrade</p>
                         <p>Ficção</p>
                     </div>
-
                 </article>
+                {livros.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <img src={item.imagem_url} alt="imagem do livro" />
+                            <div>
+                                <h4>{item.titulo}</h4>
+                                <p>{item.autor}</p>
+                                <p>{item.categoria}</p>
+                            </div>
+                        </article>
+                    );
+                })}
+
             </section>
         </section>
     );
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,22 +1,63 @@
 import S from './queroDoar.module.scss';
 import FORM_LIVRO from '../../assets/form-livro.png';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 export default function QueroDoar() {
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+
+    const enviarDados = async() => {
+        const urlApi = "https://testeapiflask-reox.onrender.com/doar"
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+        alert("Livro doado com sucesso!")
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+    const capturaTitutlo = (e) => {
+        setTitulo(e.target.value)
+    }
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+    const capturaImagem = (e) => {
+        setImagem_url(e.target.value)
+    }
+
     return (
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário 
                     com suas informações e as informações do Livro</h2>
-                <form action="" className={S.forms}>
+                <form onSubmit={(e)=> e.preventDefault() } className={S.forms}>
                     <div>
                         <img src={FORM_LIVRO} alt="icone do livro do forms" />
                         <h3>Informações do Livro</h3>
                     </div>
-                    <input type="text" placeholder='Titulo'/>
-                    <input type="text" placeholder='Categoria'/>
-                    <input type="text" placeholder='Autor'/>
-                    <input type="text" placeholder='Link da Imagem'/>
-                    <input type="submit" value="Doar" />
+                    <input type="text" placeholder='Titulo' onChange={capturaTitutlo} value={titulo}/>
+                    <input type="text" placeholder='Categoria' onChange={capturaCategoria} value={categoria}/>
+                    <input type="text" placeholder='Autor' onChange={capturaAutor} value={autor}/>
+                    <input type="text" placeholder='Link da Imagem' onChange={capturaImagem} value={imagem_url}/>
+                    <input type="submit" value="Doar" onClick={enviarDados} />
 
                 </form>    
             </section>
